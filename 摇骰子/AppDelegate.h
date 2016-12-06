911ba025925bee00911adf04e6795d8589c8b8b9
残月 <632342527@qq.com>
2016-12-06 10:36:14 +0800

    Initial Commit

---
+++ 摇骰子/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  摇骰子
+//
+//  Created by 王奥东 on 16/12/6.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 摇骰子/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  摇骰子
+//
+//  Created by 王奥东 on 16/12/6.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
